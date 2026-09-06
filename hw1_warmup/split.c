8c5d969d4bc1b3b51b3602fef5d0711d6bd1879a
Brant Willis <gbx3yr@virginia.edu>
2026-09-06 16:02:50 -0400

    push

--- hw1_warmup/split.c
+++ hw1_warmup/split.c
@@ -5,9 +5,39 @@
 
 char **string_split(const char *input, const char *sep, int *num_words) {
     char **output = NULL;
-    char *input_copy = strcopy(input);
+    char *input_copy[100];
+    strcpy(input, input_copy);
     
     char *substring = malloc(sizeof(char) * 5);
-    *num_words += 1;
+    //*num_words += 1;
+
+    int sep_length = 1;
+    int input_length = 5;
+
+    int first_comp_flag = 0;
+
+    int i = 0;
+    int j = 0;
+
+    char *temp = malloc(sizeof(char));
+
+    while (strcmp(input_copy[i], '\0') != 0) {
+        if (strcmp(input_copy[i], sep[0]) != 0) {
+            first_comp_flag = 0;
+            temp = realloc(temp, sizeof(temp) + sizeof(char));
+            strcat(temp, input_copy[i]);
+        }
+        else if (first_comp_flag == 0) {
+            first_comp_flag = 1;
+            output[j] = malloc(sizeof(temp));
+            strcpy(output[0], temp);
+            free(temp);
+            temp = malloc(sizeof(char));
+            j += 1;
+            *num_words += 1;
+        }
+        i += 1;
+    }
+
 
 }
