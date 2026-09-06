61f4de2fbfc883d408874c595828c1dc0ee2ef33
Brant Willis <gbx3yr@virginia.edu>
2026-09-06 18:31:33 -0400

    finished hw?

--- hw1_warmup/main.c
+++ hw1_warmup/main.c
@@ -1,35 +1,45 @@
-//#include "split.h"
+#include "split.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    char buffer[5000];
+    //initialize buffer and sep for function input
+    char buffer[5001];
+    char sep[5001] = "";
 
-    char sep[100];
-
+    //concatenate arguments, or set default sep
     int count = 1;
     while(count < argc) {
         strcat(sep, argv[count]);
         count += 1;
     }
-
     if (count == 1) {
         strcpy(sep, " \t");
     }
 
-    puts(sep);
-
     while (1) {
+        //read stdin to buffer
         fgets(buffer, sizeof(buffer), stdin);
-        puts(buffer);
 
-        //printf("%d %d\n", strncmp(buffer, "."), strcmp(buffer, "what"));
-        if(strncmp(buffer, ".", 1) == 0) break;
+        //if there is a newline, replace it with null terminator
+        if (strlen(buffer) > 0 && buffer[strlen(buffer) - 1] == '\n') {
+            buffer[strlen(buffer) - 1] = '\0';
+        }
 
-        int *num;
-        //char **result = string_split(buffer, sep, num);
-        //then print **result
+        //exit if input is "."
+        if(strcmp(buffer, ".") == 0) break;
+
+        //initialize variables and call function
+        int amt = 0;
+        char **result = string_split(buffer, sep, &amt);
+
+        //print and free results
+        for (int i = 0; i < amt; i++) {
+            printf("[%s]", result[i]);
+            free(result[i]);
+        }
+        free(result);
     }
 
     return 0;
--- hw1_warmup/split.c
+++ hw1_warmup/split.c
@@ -4,40 +4,68 @@
 #include <stdlib.h>
 
 char **string_split(const char *input, const char *sep, int *num_words) {
+    //initialize output array and set counter to 0
     char **output = NULL;
-    char *input_copy[100];
-    strcpy(input, input_copy);
-    
-    char *substring = malloc(sizeof(char) * 5);
-    //*num_words += 1;
+    *num_words = 0;
 
-    int sep_length = 1;
-    int input_length = 5;
+    //copy input for iteration
+    char *input_copy = malloc(strlen(input) + 1);
+    strcpy(input_copy, input);
 
+    //flag to track if a found separator character is the first in a row
     int first_comp_flag = 0;
 
+    //input iterator
     int i = 0;
-    int j = 0;
 
-    char *temp = malloc(sizeof(char));
+    //temporary string to build for each 
+    char *temp = malloc(1);
+    temp[0] = '\0';
 
-    while (strcmp(input_copy[i], '\0') != 0) {
-        if (strcmp(input_copy[i], sep[0]) != 0) {
-            first_comp_flag = 0;
-            temp = realloc(temp, sizeof(temp) + sizeof(char));
-            strcat(temp, input_copy[i]);
+    //set all separator characters to the first one for easy checking
+    //  note that only input_copy is changed, not input
+    while (input_copy[i] != '\0') {
+        for (int e = 0; e < strlen(sep); e++) {
+            if (input_copy[i] == sep[e]) {
+                input_copy[i] = sep[0];
+            }
         }
+        i += 1;
+    }
+
+    //iterate through input
+    i = 0;
+    while (input_copy[i] != '\0') {
+        //if the current character is not a separator, add it to temp
+        if (input_copy[i] != sep[0]) {
+            first_comp_flag = 0;
+            temp = realloc(temp, strlen(temp) + 2);
+            strncat(temp, input_copy + i, 1);
+        }
+        //if the current character is the first separator after
+        //a non-separator, add temp to the output, reset temp, and
+        //increment num_words
         else if (first_comp_flag == 0) {
             first_comp_flag = 1;
-            output[j] = malloc(sizeof(temp));
-            strcpy(output[0], temp);
+            output = realloc(output, sizeof(char*) * (*num_words + 1));
+            output[*num_words] = strdup(temp);
+
             free(temp);
-            temp = malloc(sizeof(char));
-            j += 1;
+            temp = malloc(1);
+            temp[0] = '\0';
             *num_words += 1;
         }
         i += 1;
     }
 
+    //move current temp to output
+    output = realloc(output, sizeof(char*) * (*num_words + 1));
+    output[*num_words] = strdup(temp);
+    *num_words += 1;
 
+    //free memory that shouldn't exit the function
+    free(temp);
+    free(input_copy);
+
+    return output;
 }
